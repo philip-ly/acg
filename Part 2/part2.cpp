c21c1d9a6e21d4fee149a9ac803156c43aa9e8b2
Philip <32490263+anfantanion@users.noreply.github.com>
2021-03-14 15:08:00 +0000

    Part 2 Fix Mult and Div Bounds

--- Part 2/part2.cpp
+++ Part 2/part2.cpp
@@ -2,14 +2,26 @@
 #include "part2.h"
 
 int main(int argc, const char * argv[]) {
-    typedef ADD<VAR<BOUNDS<10,10>>,MUL<SUB<VAR<BOUNDS<10,10>>,LIT<2>>,SUB<VAR<BOUNDS<10,10>>,LIT<3>>>> FORMULA;
-    // printf("Hi");
-    typedef ADD<LIT<10>,VAR<BOUNDS<5,10>>> shorttest;
+    typedef 
+    ADD<
+        VAR<BOUNDS<-5,10>>,
+        MUL<
+            SUB<
+                VAR<BOUNDS<-5,10>>,
+                LIT<2>>,
+            SUB<
+                VAR<BOUNDS<-5,10>>,
+                LIT<3>
+                >
+            >
+        >
+    FORMULA;
+    typedef ADD<LIT<10>,VAR<BOUNDS<-1,10>>> shorttest;
+    typedef DIV<LIT<10>,VAR<BOUNDS<-1,10>>> shorttest2;
 
     int x = shorttest::lower;
     
-    printf("Lower: %d\nUpper: %d \n", shorttest::lower , shorttest::upper);
-    printf("Answer: %d\n", (shorttest::eval(8)));
-    //typedef ADD<LIT<10>,LIT<10>>
-    // printf("Static %d", (EVAL<10,FORMULA>::RET));
+    printf("Lower: %d\nUpper: %d \n", shorttest2::lower , shorttest2::upper);
+    printf("Answer: %d\n", (shorttest2::eval(1)));
+    //static_assert(FORMULA::eval(0) == 6);
 };
